3a177b7c8345471d45da57a1f6a53f4d4c438b50
Marisa Clardy <marisa@clardy.eu>
2021-04-11 15:12:59 -0500

    Fix jest and include some more config from snowpack.

--- record/jest.config.js
+++ record/jest.config.js
@@ -1,26 +1,25 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const snowpackJest = require('@snowpack/app-scripts-react/jest.config.js');
+
 module.exports = {
+  ...snowpackJest(),
   clearMocks: true,
-  setupFiles: [
+  setupFilesAfterEnv: [
     '<rootDir>/prepare.ts',
   ],
   testMatch: [
     '<rootDir>/src/**/?(*.)+(spec|test).[tj]sx',
-    '<rootDir>/src/**/?(*.)+(spec|test).[tj]s',
+    '<rootDir>/src/**/?*.(spec|test).[tj]s',
   ],
   testPathIgnorePatterns: [
     '<rootDir>/node_modules/',
-    '<rootDir>/../node_modules/',
-    '<rootDir>/public/',
-    '<rootDir>/.next/',
-    '<rootDir>/out/',
+    '<rootDir>/dist/',
   ],
-  moduleDirectories: ['<rootDir>/src', '<rootDir>/node_modules', '<rootDir>/../node_modules', '<rootDir>'],
+  moduleDirectories: ['<rootDir>/src', '<rootDir>/node_modules', '<rootDir>/../node_modules'],
   moduleFileExtensions: [
     'js',
     'json',
     'ts',
-    'tsx',
-    'jsx',
   ],
   testURL: 'http://localhost',
   snapshotResolver: '<rootDir>/snapshotResolver.js',
