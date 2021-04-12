549dad1bd45747e62332ba7942ac4fb71bf8561d
Marisa Clardy <marisa@clardy.eu>
2021-04-11 22:39:26 -0500

    Add reset command and fix some

--- gramophone/src/commands/migrate/migrate.spec.ts
+++ gramophone/src/commands/migrate/migrate.spec.ts
@@ -3,17 +3,24 @@ import { existsSync } from 'fs';
 import { join } from 'path';
 import up from './up';
 import rollback from './rollback';
-// import reset from './reset';
+import reset from './reset';
 import create from './create';
 import db from '../../setup/db';
 
+const { log } = console;
+
 describe('migrate', () => {
+  beforeAll(() => { console.log = jest.fn(); });
+  afterAll(() => { console.log = log; });
+
   afterEach(() => rollback());
+
   describe('up', () => {
     it('can run migrations', async () => {
       await up();
       const count = await db('migrations').count('migrationId');
       expect(count[0]['count(`migrationId`)']).toBeGreaterThan(0);
+      expect(console.log).toHaveBeenCalled();
     });
   });
   describe('rollback', () => {
@@ -21,15 +28,28 @@ describe('migrate', () => {
       await up();
       const count = await db('migrations').count('migrationId');
       expect(count[0]['count(`migrationId`)']).toBeGreaterThan(0);
+      expect(console.log).toHaveBeenCalled();
 
       await rollback();
       const count2 = await db('migrations').count('migrationId');
       expect(count2[0]['count(`migrationId`)']).toBe(0);
+      expect(console.log).toHaveBeenCalled();
+    });
+  });
+  describe('reset', () => {
+    it('empties database', async () => {
+      await up();
+      const count = await db('migrations').count('migrationId');
+      expect(count[0]['count(`migrationId`)']).toBeGreaterThan(0);
+      await reset();
+      const migrationsExist = await db.schema.hasTable('migrations');
+      expect(migrationsExist).toBe(false);
     });
   });
 
   describe('create', () => {
     it('creates a migration file with snake_case param', async () => {
+      Date.now = jest.fn(() => 1618196206380);
       await create('migration_test');
       const path = join(__dirname, '..', '..', 'schema', 'MigrationTest.ts');
       expect(existsSync(path)).toBeTruthy();
@@ -44,10 +64,12 @@ describe('migrate', () => {
       expect(file).toMatch(/export const down = async/);
       expect(file).toMatch(/export const timestamp = \d+;/);
       expect(file).toMatchSnapshot();
+      expect(console.log).toHaveBeenCalled();
       await rm(path);
     });
 
     it('creates a migration file with PascalCase param', async () => {
+      Date.now = jest.fn(() => 1618196206380);
       await create('MigrationTest');
       const path = join(__dirname, '..', '..', 'schema', 'MigrationTest.ts');
       expect(existsSync(path)).toBeTruthy();
--- gramophone/src/commands/migrate/reset.ts
+++ gramophone/src/commands/migrate/reset.ts
@@ -1,3 +1,25 @@
+import chalk from 'chalk';
+import { readdir } from 'fs/promises';
+import { join } from 'path';
+import db from '../../setup/db';
+import { Migration } from './types';
+
 export default async () => {
-  console.error('TODO: Implement me!!!!');
+  if (!(await db.schema.hasTable('migrations'))) {
+    console.log('Nothing to reset.');
+    return;
+  }
+
+  const dir = join(__dirname, '..', '..', 'schema');
+  const files = await readdir(dir);
+  // eslint-disable-next-line
+  const promises = files.map((file) => require(`${dir}/${file}`) as Migration)
+    .sort((a: Migration, b: Migration) => (a.timestamp > b.timestamp ? -1 : 1))
+    .map(async (schema: Migration) => {
+      const { down } = schema;
+      await down();
+    });
+  await Promise.all(promises);
+  await db.schema.dropTableIfExists('migrations');
+  console.log(chalk.yellow('\n\t⚠️  Rolled back all migrations. ⚠️\n'));
 };
