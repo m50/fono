9497c9016de3756484b8368ca6e873ebec04ace9
Marisa Clardy <marisa@clardy.eu>
2021-04-16 18:49:50 -0500

    Password auth should only happen at login, remove it from auth middleware.

--- gramophone/src/endpoints/login/index.ts
+++ gramophone/src/endpoints/login/index.ts
@@ -1,17 +1,21 @@
 import { FastifyInstance, FastifyPluginCallback, RouteShorthandOptions } from 'fastify';
-import { auth } from 'middleware/auth';
+import { AuthParams } from 'middleware/auth/types';
+import { refreshToken } from 'middleware/auth/utils';
+import { passwordAuth } from './passwordAuth';
 import schema from './Body.schema.json';
 
 export const register: FastifyPluginCallback<{}> = (app: FastifyInstance, _, done) => {
-  auth(app);
-
   const opts: RouteShorthandOptions = { schema: { body: schema } };
-  app.post('/login', opts, async (req, reply) => {
-    if (!req.user) {
+  app.post<AuthParams>('/login', opts, async (req, reply) => {
+    const user = await passwordAuth(req);
+    if (!user) {
       reply.status(401);
       return { message: 'Authentication failed' };
     }
 
+    const token = refreshToken(user);
+
+    reply.header('X-Refresh-Token', token);
     reply.status(200);
     return { message: 'Successfully logged in!' };
   });
