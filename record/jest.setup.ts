4b9d31eb2600e6e422fe332733b6d6d00c026e87
Marisa Clardy <marisa@clardy.eu>
2021-04-17 14:13:58 -0500

    switch to node-fetch from whatwg-fetch. Node-fetch works with fake timers in jest, whatwg-fetch does not.

--- record/jest.setup.ts
+++ record/jest.setup.ts
@@ -1,3 +1,10 @@
 /* eslint-disable */
-import 'whatwg-fetch';
 import '@testing-library/jest-dom/extend-expect';
+import fetch from 'node-fetch';
+
+// @ts-ignore
+window.fetch = (url: string, ...rest) => fetch(
+  /^https?:/.test(url) ? url : new URL(url, 'http://localhost'),
+  // @ts-ignore
+	...rest
+);
