01a9ac8543a7f6a1cab16924f7f93cd5450b2bb6
Marisa Clardy <marisa@clardy.eu>
2021-04-17 12:29:49 -0500

    Add tests for card component.

---
+++ record/src/components/card/card.snap.tsx
@@ -0,0 +1,27 @@
+// Jest Snapshot v1, https://goo.gl/fbAQLP
+
+exports[`Card matches snapshot 1`] = `
+<DocumentFragment>
+  <section
+    class="bg-gray-400 text-white bg-opacity-40 p-5 rounded-xl shadow-lg relative"
+  >
+    <div
+      class="absolute left-0 right-0 top-0 bottom-0 rounded-xl z-0 pointer-events-none bg-gradient-to-tr from-transparent to-transparent via-white opacity-30 dark:via-gray-400"
+    />
+    <main
+      class="flex flex-col justify-between items-center space-y-5 z-10"
+    >
+      <div
+        class="w-full"
+      >
+        Body
+      </div>
+      <div
+        class="w-full"
+      >
+        Body
+      </div>
+    </main>
+  </section>
+</DocumentFragment>
+`;
---
+++ record/src/components/card/card.spec.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Card from './index';
+
+describe('Card', () => {
+  it('Can only have one title', () => {
+    render((
+      <Card>
+        <Card.Title>Title</Card.Title>
+        <Card.Title>Title</Card.Title>
+      </Card>
+    ));
+    expect(screen.getAllByText('Title')).toHaveLength(1);
+  });
+  it('Can only have one footer', () => {
+    render((
+      <Card>
+        <Card.Footer>Footer</Card.Footer>
+        <Card.Footer>Footer</Card.Footer>
+      </Card>
+    ));
+    expect(screen.getAllByText('Footer')).toHaveLength(1);
+  });
+  it('Can only have multiple bodies', () => {
+    render((
+      <Card>
+        <Card.Body>Body</Card.Body>
+        <Card.Body>Body</Card.Body>
+      </Card>
+    ));
+    expect(screen.getAllByText('Body')).toHaveLength(2);
+  });
+  it('matches snapshot', () => {
+    const { asFragment } = render((
+      <Card>
+        <Card.Body>Body</Card.Body>
+        <Card.Body>Body</Card.Body>
+      </Card>
+    ));
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
