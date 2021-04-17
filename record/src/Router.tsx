f402b1a20cf34219f34d02bb9ede6832a15a5f21
Marisa Clardy <marisa@clardy.eu>
2021-04-17 03:25:36 -0500

    Call useApi in the router, so that no matter where you are at, you will be logged out if need be.

--- record/src/Router.tsx
+++ record/src/Router.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Home from 'pages';
 import type { RouteComponentProps } from '@reach/router';
+import useApi from 'hooks/useApi';
 
 // eslint-disable-next-line
 const Router = (props: RouteComponentProps) => {
+  useApi();
   return (
     <>
       <Home path="/" />
