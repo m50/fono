ee52da7ad1a16012e715bb6ae2084577d83e27ae
Marisa Clardy <marisa@clardy.eu>
2021-04-17 02:43:23 -0500

    Fix some backend issues found while implementing frontend.

--- gramophone/src/endpoints/logout.ts
+++ gramophone/src/endpoints/logout.ts
@@ -1,7 +1,10 @@
 import { FastifyInstance, FastifyPluginCallback } from 'fastify';
+import { auth } from 'middleware/auth';
 
 export const register: FastifyPluginCallback<{}> = (app: FastifyInstance, _, done) => {
-  app.auth().get('/logout', async (req, reply) => {
+  auth(app);
+
+  app.get('/logout', async (req, reply) => {
     reply.removeHeader('X-Refresh-Token');
     reply.status(200);
     return { message: 'Successfully logged out!' };
