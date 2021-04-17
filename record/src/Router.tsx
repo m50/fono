1ef8f028c483973df1b2a5face576f109ac3e4c2
Marisa Clardy <marisa@clardy.eu>
2021-04-17 02:57:44 -0500

    Remove useAuth references that I am no longer using.

--- record/src/Router.tsx
+++ record/src/Router.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import useAuth from 'hooks/useAuth';
 import Home from 'pages';
 import type { RouteComponentProps } from '@reach/router';
 
 // eslint-disable-next-line
 const Router = (props: RouteComponentProps) => {
-  useAuth();
   return (
     <>
       <Home path="/" />
