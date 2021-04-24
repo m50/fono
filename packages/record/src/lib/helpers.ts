d4a165ebc78bddea146fc94a0130b3e496a4e675
Marisa Clardy <marisa@clardy.eu>
2021-04-24 18:41:02 -0500

    Shorten up `cl`.

--- packages/record/src/lib/helpers.ts
+++ packages/record/src/lib/helpers.ts
@@ -3,13 +3,7 @@ import slugify from 'slugify';
 export const slug = (str: string) => slugify(str, { lower: true, strict: true, locale: 'en' });
 export const isProduction = (): boolean => import.meta.env.NODE_ENV === 'production';
 export const isDev = (): boolean => !isProduction();
-export const cl = (strings: TemplateStringsArray, ...expr: string[]): string => {
-  let str = '';
-  strings.forEach((string, i) => {
-    str += string + (expr[i] || '');
-  });
-
-  str = str.replace(/\s+/g, ' ').trim();
-
-  return str;
-};
+export const cl = (strings: TemplateStringsArray, ...expr: string[]): string => strings
+  .reduce((prev, cur, idx) => prev + cur + (expr[idx] ?? ''), '')
+  .replace(/\s+/g, ' ')
+  .trim();
