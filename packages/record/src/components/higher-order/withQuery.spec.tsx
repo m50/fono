7c175c7fd78cf2ad5e7135e0826d37c32724b6d5
Marisa Clardy <marisa@clardy.eu>
2021-05-07 01:13:51 -0500

    Set a default auth token.

--- packages/record/src/components/higher-order/withQuery.spec.tsx
+++ packages/record/src/components/higher-order/withQuery.spec.tsx
@@ -4,6 +4,7 @@ import { render, waitFor } from '@testing-library/react';
 import { graphql } from 'msw';
 import { withQuery } from './withQuery';
 import { withProviders } from './withProviders';
+import { DateTime } from 'luxon';
 
 interface UserExample {
   name: string;
@@ -18,17 +19,28 @@ const mockNavigate = jest.fn();
 jest.mock('@reach/router', () => ({
   useNavigate: () => mockNavigate,
 }));
+const token = { u: 1, t: Date.now(), k: '1234567890', e: DateTime.now().plus({ hour: 1 }).toMillis() };
+const refreshToken = btoa(JSON.stringify(token));
 const server = setupServer(
   graphql.query<{ user: UserExample }>(
     'GetUser',
-    (req, res, ctx) => res(ctx.data({ user: { name: 'John' } })),
+    (req, res, { data, set }) => res(
+      set('X-Refresh-Token', refreshToken),
+      data({ user: { name: 'John' } }),
+    ),
   ),
 );
 
 const Example = (props: ExampleProps) => (<div className={props.className}>{props.user.name}</div>);
 
 describe('withQuery()', () => {
-  beforeAll(() => server.listen());
+  beforeAll(() => server.listen({ onUnhandledRequest: 'error' }));
+  beforeEach(() => {
+    server.resetHandlers();
+    mockNavigate.mockReset();
+    localStorage.clear();
+    localStorage.setItem('jwt', JSON.stringify(token));
+  });
   afterAll(() => server.close());
 
   it('fills out props', async () => {
